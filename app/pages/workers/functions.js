adbbf08d2f6d7a7fe2ef8b08eacffe902308292f
Alon Zusman <aloniko.zus@gmail.com>
2016-09-04 20:36:50 +0300

    Selecting skils and fix name random selection

--- app/pages/workers/functions.js
+++ app/pages/workers/functions.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import getRandomInt from '../../global/functions.js';
 
 import names from './names.json';
+import skills from './../../global/skills.json';
 
 export default addWorker = function () {
+
+    // Selecting random name
+
     var gerner = "male";
+    var firstName = names["first"][gender][getRandomInt(0,names["first"][gender].length)];
+    var lastName = names["last"][gender][getRandomInt(0,names["last"].length)];
 
-    var numOfNames = toString(Math.floor(names["first"][gender].length)).length * 10;
-    var firstName = names["first"][gender][Math.round(Math.random()*numOfNames)];
-
-    var numOfNames = toString(Math.floor(names["last"].length)).length * 10;
-    var lastName = names["last"][gender][Math.round(Math.random()*numOfNames)];
-
-     // Create a rundom name
+    // Selecting skills
+    var initSkills = 2; //number of skills to select at first init
+    var chosen = [];
+    var skills = [];
+    for (var index = 0; index < initSkills; index++) {
+        var rnd = getRandomInt(0,initSkills);
+        while (chosen[rnd]  != undefined) {
+             rnd = getRandomInt(0,initSkills);
+        }
+        chosen[rnd] = 'c';
+        skills[index] = {
+            "name": skills[rnd]["name"],
+            "rank": getRandomInt(skills[rnd]["startingPointsMin"],skills[rnd]["startingPointsMax"])
+        };
+    }
 }
