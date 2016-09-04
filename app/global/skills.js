ccccbe825d719e5710f5925180f2c293f43b7180
Alon Zusman <aloniko.zus@gmail.com>
2016-09-04 17:39:35 +0300

    Added skill.js to global folder

---
+++ app/global/skills.js
@@ -0,0 +1,12 @@
+[
+    {
+        "name": "code",
+        "startingPointsMin": 1,
+        "startingPointsMax": 5
+    },
+    {
+        "name": "art",
+        "startingPointsMin": 1,
+        "startingPointsMax": 5
+    }
+]
